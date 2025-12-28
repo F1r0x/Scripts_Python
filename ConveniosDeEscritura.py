ec83ddf5d98a0934152e01c32f7651c6a720f60b
Th3F1r0x <patrickschonewolf@hotmail.com>
2025-12-28 01:27:29 +0100

    Python Scripts

---
+++ ConveniosDeEscritura.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# CONVENIOS DE DE ESCRITURA
+
+#loweCamelCase
+def comprobacionEstadoApi():
+
+# UpperCamelCase
+class TwitterApi:
+
+    # SCREAMING_SNAKE_CASE
+
+    VERSION_API = 1
+    URL_API = "https://hack4u.io"
+
+    _protegido = "Estoy protegido."
+
+# PEP 8
+
+
+# snake_case
+def comprobar_estado_api():
---
+++ SoyElModuloPrincipal.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Se utiliza para identificar el módulo principal.
+if __name__ == '__main__': 
+    print("Hola, soy el módulo principal")
+
+else:
+    print("No soy el módulo principal")
+
