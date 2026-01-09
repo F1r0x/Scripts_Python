0037e1b15af62fd6379b6740d56d62f24cbd43bc
Th3F1r0x <patrickschonewolf@hotmail.com>
2026-01-09 01:00:11 +0100

    Finalizado Clases y Objetos en Python

--- CursoPython12-ClasesObjetos/ClasesObjetos6.py
+++ CursoPython12-ClasesObjetos/ClasesObjetos6.py
@@ -3,6 +3,7 @@
 class Libro:
 
     bestseller_value = 5000
+    IVA = 0.21
 
     def __init__(self, titulo, autor, precio):
         
@@ -12,9 +13,24 @@ class Libro:
 
     @staticmethod # Decoradores
     def es_bestseller(total_ventas): # Libro.es_bestseller(mi_libro, total_ventas)
-        
         return total_ventas > Libro.bestseller_value
+    
+    @classmethod # Decoradores
+    def precio_con_iva(cls, precio):
+        return precio + precio * cls.IVA
+    
+class LibroDigital(Libro): # Hereda las propiedades de la clase Libro
+    IVA = 0.10
 
-mi_libro = Libro("Cómo ser un Lammer", "Marcelo", "17.5")
+
+mi_libro = Libro("Cómo ser un Lammer", "Marcelo", 17.5)
+mi_libro_digital = LibroDigital("Cómo ser un HAcker", "Marcelo", 17.5)
+
+
+
 print(f"\n [+] Título: {mi_libro.titulo}")
+print(f"\n [+] Precio con IVA redondeado: {round(Libro.precio_con_iva(mi_libro.precio),2)}€") # Utiliza round para redondear a 2 cifras 
+print(f"\n [+] Precio con IVA: {Libro.precio_con_iva(mi_libro.precio)}€")
+print(f"\n [+] Precio con IVA digital: {LibroDigital.precio_con_iva(mi_libro_digital.precio)}€")
+
 print(f"\n [+] Es un Bestseller? {Libro.es_bestseller(2000)}")
