2a5002ffa1eccb5cc0eb94f9bc5ab38fb97a0731
Th3F1r0x <patrickschonewolf@hotmail.com>
2026-01-12 12:52:27 +0100

    New Course

---
+++ CursoPython14-UsoDelSelf/ejercicio3.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Clase Calculadora
+class Calculadora:
+
+    # Constructor
+    def __init__(self, valor):
+       self.valor = valor
+
+
+    # Suma
+    def suma(self, valor2):
+        print(self.valor + valor2)
+
+    def resta(self, valor2):
+        print(self.valor - valor2)
+    
+    def mult(self, valor2):
+        print(self.valor * valor2)
+    
+    def div(self, valor2):
+        print(self.valor / valor2 if valor2 != 0 else f"\n[!] Error: No se puede dividir entre 0")
+
+
+calc = Calculadora(5)
+calc.suma(5)
+calc.resta(5)
+calc.mult(5)
+calc.div(5)
+calc.div(0)
